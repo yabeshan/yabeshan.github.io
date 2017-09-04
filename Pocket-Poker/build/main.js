6a120b1085c703221a564e5db33b8137975596b3
beshan <yabeshan@ya.ru>
2017-09-04 19:18:45 +0300

    test rotate

--- Pocket-Poker/build/main.js
+++ Pocket-Poker/build/main.js
@@ -570,9 +570,6 @@ var CpBoard = (function () {
         else {
             this.top = (h - 600 * this.zoom) * 0.5 / this.zoom;
         }
-        if (!isLandscape) {
-            alert(w + "____" + (800 * this.zoom) + "-----" + h + "____" + (600 * this.zoom));
-        }
     };
     CpBoard.prototype.mobilePosition = function (w, h, isLandscape) {
         var o = window.orientation + "";
@@ -610,7 +607,7 @@ var CpBoard = (function () {
 }());
 CpBoard = __decorate([
     Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["n" /* Component */])({
-        selector: 'cp-board',template:/*ion-inline-start:"D:\Nick_work\poker\Pocket-Poker\src\components\cp-board\cp-board.html"*/'<div class="board-container" \n\n    [style.zoom]="zoom" \n\n    [style.transform]="\'rotate(\' + rotate + \'deg)\'" \n\n    [style.top.px]="top"\n\n    [style.left.px]="left">\n\n    \n\n    <div class="cards-holder">\n\n            <!-- cards-holder -->\n\n    </div>\n\n    <div class="room-holder">\n\n        <cp-room></cp-room>\n\n    </div>\n\n\n\n</div>'/*ion-inline-end:"D:\Nick_work\poker\Pocket-Poker\src\components\cp-board\cp-board.html"*/
+        selector: 'cp-board',template:/*ion-inline-start:"D:\Nick_work\poker\Pocket-Poker\src\components\cp-board\cp-board.html"*/'<!-- <div class="board-container" \n\n    [style.zoom]="zoom" \n\n    [style.transform]="\'rotate(\' + rotate + \'deg)\'" \n\n    [style.top.px]="top"\n\n    [style.left.px]="left"> -->\n\n    \n\n<div class="board-container" \n\n    [style.zoom]="zoom" \n\n    [style.top.px]="top"\n\n    [style.left.px]="left">\n\n\n\n    <div class="cards-holder">\n\n            <!-- cards-holder -->\n\n    </div>\n\n    <div class="room-holder">\n\n        <cp-room></cp-room>\n\n    </div>\n\n\n\n</div>'/*ion-inline-end:"D:\Nick_work\poker\Pocket-Poker\src\components\cp-board\cp-board.html"*/
     }),
     __metadata("design:paramtypes", [__WEBPACK_IMPORTED_MODULE_1_ionic_angular__["d" /* Platform */]])
 ], CpBoard);
