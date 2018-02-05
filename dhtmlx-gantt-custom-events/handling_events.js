3ed2935a05000f4b2e763d2ffa0fe6864670aaa6
beshan <yabeshan@ya.ru>
2018-02-05 09:33:30 +0200

    update gantt

--- dhtmlx-gantt-custom-events/handling_events.js
+++ dhtmlx-gantt-custom-events/handling_events.js
@@ -1,16 +1,26 @@
+var ganttEditTaskId;
+var ganttEditTaskItemId;
+var ganttEditTaskItemValue;
+
 gantt.attachEvent("onTaskClick", function(id, event) {
     hideContextMenu(event);
     editItemDisabled();
-    selectItem(event);
+    // selectItem(event);
     return true;
 });
 
-gantt.attachEvent("onTaskDblClick", function(id, e) {
+gantt.attachEvent("onTaskDblClick", function(idTask, event) {
+    ganttEditTaskId = idTask;
+    ganttEditTaskItemId = getItemId(idTask, event);
+    ganttEditTaskItemValue = getElement(event).innerText;
+
+    hideContextMenu(event);
     editItem(event);
     return false;
 });
 
 gantt.attachEvent("onContextMenu", function(taskId, linkId, event){
+    editItemDisabled();
     showContextMenu(event);
     return false;
 });
@@ -18,6 +28,29 @@ gantt.attachEvent("onContextMenu", function(taskId, linkId, event){
 
 /************ UTILS ***********/
 var init = true;
+
+function getItemId(idTask, event) {
+    var el = getElement(event);
+    var parent = (el.className == "gantt_cell") ? event.target.parentElement : event.target.parentElement.parentElement;
+    var child = (el.className == "gantt_cell") ? event.target : event.target.parentElement;
+    var arr = parent.childNodes;
+    var selectId = 0;
+    for (var k=0; k<arr.length; k++) {
+        if (arr[k] == child) {
+            selectId = k;
+            break;
+        }
+    }
+
+    return selectId;
+}
+
+function getElement(event) {
+    if (event.target) {
+        return event.target;
+    }
+    return event.srcElement;
+}
 
 function editItemDisabled() {
     var item = document.getElementById("edit-item");
@@ -27,11 +60,31 @@ function editItemDisabled() {
 function changeEditInput() {
     var inp = document.getElementById("edit-item-input");
     inp.addEventListener("keydown", function(event) {
-        if (event.key === "Enter" || event.key === "Escape") {
+        if (event.key === "Enter") {
+            saveEditInput();
+            event.preventDefault();
+        }
+
+        if (event.key === "Escape") {
             editItemDisabled();
             event.preventDefault();
         }
     });
+}
+
+function saveEditInput() {
+    var task = gantt.getTask(ganttEditTaskId);
+    var inp = document.getElementById("edit-item-input");
+    var type = ["text", "start_date", "duration", "end_date", "priority", "option", "option"];
+    if (ganttEditTaskItemId==1 || ganttEditTaskItemId==3) {
+        task[ type[ganttEditTaskItemId] ] = new Date(inp.value);
+    } else {
+        task[ type[ganttEditTaskItemId] ] = inp.value;
+    }
+    gantt.updateTask(ganttEditTaskId); 
+    gantt.refreshData();
+    
+    editItemDisabled();
 }
 
 function editItem(event) {
@@ -49,7 +102,7 @@ function editItem(event) {
     item.style.display = "block";
     
     var inp = document.getElementById("edit-item-input");
-    inp.value = event.srcElement.innerText;
+    inp.value = ganttEditTaskItemValue;
     inp.focus();
 }
 
@@ -63,7 +116,7 @@ function selectItem(event) {
 }
 
 function selectItemValid(event) {
-    var cl = event.srcElement.className;
+    var cl = getElement(event).className;
     if (cl.indexOf('gantt_add')>=0 || cl.indexOf('gantt_last_cell')>=0) {
         return false;
     }
@@ -71,14 +124,17 @@ function selectItemValid(event) {
 }
 
 function selectItemPosition(event, item, padding) {
-    item.style.top = event.clientY - event.layerY - padding + "px";
-    item.style.left = event.clientX - event.layerX - padding + "px";
+    var offY = event.offsetY; //event.layerY;
+    var offX = event.offsetX; //event.layerX;
+    item.style.top = event.clientY - offY - padding + "px";
+    item.style.left = event.clientX - offX - padding + "px";
 }
 
 function selectItemSize(event, item) {
-    var w = (event.srcElement.className == "gantt_cell") 
-        ? event.srcElement.clientWidth : event.srcElement.parentElement.clientWidth ;
-    var h = event.srcElement.clientHeight;
+    var el = getElement(event);
+    var w = (el.className == "gantt_cell") 
+        ? el.clientWidth : el.parentElement.clientWidth ;
+    var h = el.clientHeight;
     item.style.width = w + "px";
     item.style.height = h + "px";
 }
